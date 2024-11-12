dc3f7632de545c14f401e1499b47c3ac58d8d8c6
Abhisheksingh1812 <abhishekkumar@natlov.com>
2024-11-12 22:42:41 +0530

    update code

--- plantCare/src/main/java/com/example/plantCare/entity/Growth.java
+++ plantCare/src/main/java/com/example/plantCare/entity/Growth.java
@@ -1,9 +1,6 @@
 package com.example.plantCare.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +11,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@Table(name="Growth")
 public class Growth {
 
     @Id
--- plantCare/src/main/java/com/example/plantCare/entity/User.java
+++ plantCare/src/main/java/com/example/plantCare/entity/User.java
@@ -1,9 +1,6 @@
 package com.example.plantCare.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +12,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@Table(name="\"user\"")
 public class User {
 
     @Id
